bdbdd2c1b7d33685ea3d0e94757af1b410c6e6c5
arplight <muhammad96osama@gmail.com>
2023-12-29 02:46:32 +0200

    Updated

--- src/Template.jsx
+++ src/Template.jsx
@@ -1,3 +1,9 @@
+import { IoIosArrowForward } from "react-icons/io";
+import { TiPlus, TiMinus } from "react-icons/ti";
+import { IoClose } from "react-icons/io5";
+import { IoTrashBinOutline } from "react-icons/io5";
+import { IoIosArrowUp } from "react-icons/io";
+
 const Template = () => {
   return (
     <>
@@ -14,7 +20,46 @@ const Template = () => {
       <p className="large-paragrapgh font-primary">App</p>
       {/* Small paragraphs */}
       <p className="small-paragrapgh font-primary">App</p>
-      <button className="font-primary">App</button>
+      {/* Buttons */}
+      {/* main button */}
+      <button className="main-button font-secondary">
+        Learn more <IoIosArrowForward className="text-[20px] button-arrow" />
+      </button>
+      <br />
+      {/* full button */}
+      <button className="full-button font-secondary">App</button>
+      {/* hollow button */} <br />
+      <button className="font-primary hollow-button">
+        App <IoIosArrowForward className="text-[20px] button-arrow" />
+      </button>
+      {/* arrow button */}
+      <button className="font-primary arrow-button">
+        Shop now <IoIosArrowForward className="text-[20px] button-arrow" />
+      </button>
+      {/* dark button */}
+      <button className="dark-button">Sign Up</button>
+      {/* quantity button */}
+      <div className="quantity-button">
+        <button className="minus border-r-[1px] border-[#12342f]">
+          <TiMinus />
+        </button>
+        <span>1</span>
+        <button className="plus border-l-[1px] border-[#12342f]">
+          <TiPlus />
+        </button>
+      </div>
+      {/* top button */}
+      <button className="top-button">
+        <IoIosArrowUp />
+      </button>
+      {/* close button */}
+      <button className="close-button">
+        <IoClose />
+      </button>
+      {/* remove button */}
+      <button className="remove-button">
+        <IoTrashBinOutline />
+      </button>
     </>
   );
 };
