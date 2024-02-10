b63d262fa7843b3f21507f9d9415d7220c877621
arplight <muhammad96osama@gmail.com>
2024-02-10 02:51:45 +0200

    Order Page

--- src/Components/pages/Order/Components/Tracking_Page/TrackingPage.jsx
+++ src/Components/pages/Order/Components/Tracking_Page/TrackingPage.jsx
@@ -1,5 +1,64 @@
+import { IoCheckmarkDoneCircleOutline } from "react-icons/io5";
+import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
+import HeroHeadings from "../../../../common/hero_headings/HeroHeadings";
+
 const TrackingPage = () => {
-  return <></>;
+  return (
+    <div>
+      <HeroHeadings
+        topHeading={"Order Tracking"}
+        bottomHeading={"Order summary"}
+        isCentered={true}
+        withStyle={"mb-3"}
+      />
+      <div className="flex w-full justify-between md:max-w-[80%] m-auto ">
+        <span className="flex flex-col gap-1 items-center">
+          <IoCheckmarkDoneCircleSharp className="text-[#12342f] text-[40px] sm:text-[60px] md:text-[80px]" />
+          <p className="small-paragrapgh font-primary">Order Placed</p>
+        </span>
+        <span className="flex flex-col gap-1 items-center">
+          <IoCheckmarkDoneCircleOutline className="text-[#12342f] text-[40px] sm:text-[60px] md:text-[80px]" />
+          <p className="small-paragrapgh font-primary">Processing</p>
+        </span>
+        <span className="flex flex-col gap-1 items-center">
+          <IoCheckmarkDoneCircleOutline className="text-[#12342f] text-[40px] sm:text-[60px] md:text-[80px]" />
+          <p className="small-paragrapgh font-primary">Recieved</p>
+        </span>
+      </div>
+      <div className="flex flex-col w-full mt-4 gap-1 md:max-w-[80%] m-auto">
+        <span className="flex gap-1">
+          <b className="large-paragrapgh font-primary text-nowrap">Order Id:</b>
+          <p className="large-paragrapgh font-primary">
+            #121234124-1252363473gfsgd-25246gsd-43634gdfdf
+          </p>
+        </span>
+        <span className="flex gap-1">
+          <b className="large-paragrapgh font-primary text-nowrap">
+            Client name:
+          </b>
+          <p className="large-paragrapgh font-primary">John doe</p>
+        </span>
+        <span className="flex gap-1">
+          <b className="large-paragrapgh font-primary text-nowrap">
+            Client email:
+          </b>
+          <p className="large-paragrapgh font-primary">user@example.com</p>
+        </span>
+        <span className="flex gap-1">
+          <b className="large-paragrapgh font-primary text-nowrap">
+            Client address:
+          </b>
+          <p className="large-paragrapgh font-primary">71 example.st krakow</p>
+        </span>
+        <span className="flex gap-1">
+          <b className="large-paragrapgh font-primary text-nowrap">
+            Total cost:
+          </b>
+          <p className="large-paragrapgh font-primary">zł 112</p>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default TrackingPage;
--- src/Components/pages/Order/Order.jsx
+++ src/Components/pages/Order/Order.jsx
@@ -5,15 +5,17 @@ import MainSection from "../../common/sections/main_section/MainSection";
 import TrackingForm from "./Components/Tracking_Form/TrackingForm";
 import TrackingPage from "./Components/Tracking_Page/TrackingPage";
 import Breadcrumb from "../../common/sections/breadcrumb/Breadcrumb";
+import Seo from "../../Seo/Seo";
 
 const Order = () => {
-  const [isFounded, setIsFounded] = useState(false);
+  const [isFounded, setIsFounded] = useState(true);
   const dispatchSpinner = useDispatch();
   useEffect(() => {
     dispatchSpinner(loadingHandler(false));
   }, [dispatchSpinner]);
   return (
     <>
+      <Seo currentPage={"Order"} currentPath={"Order"} />
       <Breadcrumb />
       <MainSection withBackground={false}>
         {isFounded ? <TrackingPage /> : <TrackingForm />}
